36e11d3c807b1aa86ee3b90419dbc4710cb96ab8
Rustam Gabdelislamov <rustamgabdelislamov911@gmail.com>
2025-10-27 16:20:22 +0500

    ok update

--- users/models.py
+++ users/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
+    username = None
     email = models.EmailField(
         unique=True, verbose_name="Email", help_text="Введите email"
     )
@@ -30,7 +31,7 @@ class CustomUser(AbstractUser):
         null=True,
     )
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username"]
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.email
