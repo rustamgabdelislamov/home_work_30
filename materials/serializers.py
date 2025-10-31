ee6f308e2866d72569b682654c6aa77ceffe5f8b
Rustam Gabdelislamov <rustamgabdelislamov911@gmail.com>
2025-10-31 20:19:40 +0500

    lesson count ok

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
 from materials.models import Course, Lesson
 
@@ -9,6 +10,17 @@ class CourseSerializer(serializers.ModelSerializer):
         model = Course
         fields = "__all__"
 
+
+class CourseDetailSerializer(serializers.ModelSerializer):
+    count_lessons = SerializerMethodField()
+
+    def get_count_lessons(self, course):
+        """Выводим количество уроков у курса по его идшнику"""
+        return Lesson.objects.filter(course=course.id).count()
+
+    class Meta:
+        model = Course
+        fields = ('name', 'description', 'count_lessons')
 
 class LessonSerializer(serializers.ModelSerializer):
 
