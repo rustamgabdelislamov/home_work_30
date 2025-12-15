01fc9030aa4c9c40488e041fc617e70a071250b0
Rustam Gabdelislamov <rustamgabdelislamov911@gmail.com>
2025-12-15 21:42:06 +0500

    Dockerfile.nginx update

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv("DEBUG") == "True" else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["158.160.207.116"]
 
 
 # Application definition
